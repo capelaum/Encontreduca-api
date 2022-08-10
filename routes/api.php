6c48064eccbfc64956586e19123dbb540cc40fc5
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-08-10 03:35:11 -0300

    ✨ Improve api routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\V1\{
-    CategoryController,
-    MotiveController,
-    ResourceChangeController,
-    ResourceComplaintController,
-    ResourceController,
-    ResourceVoteController,
-    ReviewComplaintController,
-    ReviewController,
-    SupportRequestController,
-    // UserController
-};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +8,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
-    Route::apiResource('users', UserController::class);
+    Route::apiResources([
+        'users' => UserController::class,
+        'categories' => CategoryController::class,
+        'motives' => MotiveController::class,
+        'supports' => SupportController::class,
+        'resources/complaints' => ResourceComplaintController::class,
+        'resources/changes' => ResourceChangeController::class,
+        'resources/votes' => ResourceVoteController::class,
+        'resources' => ResourceController::class,
+        'reviews/complaints' => ReviewComplaintController::class,
+        'reviews' => ReviewController::class
+    ]);
 
     Route::prefix('users')->group(function () {
         Route::delete('{user}/avatar', ['uses' => 'UserController@deleteAvatar']);
@@ -28,18 +27,4 @@ Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], f
         Route::post('/resources', ['uses' => 'UserController@storeResource']);
         Route::delete('{user}/resources/{resource}', ['uses' => 'UserController@deleteResource']);
     });
-
-    Route::apiResource('resources/complaints', ResourceComplaintController::class);
-    Route::apiResource('resources/changes', ResourceChangeController::class);
-    Route::apiResource('resources/votes', ResourceVoteController::class);
-    Route::apiResource('resources', ResourceController::class);
-
-    Route::apiResource('categories', CategoryController::class);
-    Route::apiResource('motives', MotiveController::class);
-
-
-    Route::apiResource('reviews/complaints', ReviewComplaintController::class);
-    Route::apiResource('reviews', ReviewController::class);
-
-    Route::apiResource('supports', SupportController::class);
 });
