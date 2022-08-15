bf1e04e4dcb5f3f03a15a151446482f8466d5b4b
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-08-14 23:45:58 -0300

    feat: Resource Factory

---
+++ database/factories/ResourceFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Resource>
+ */
+class ResourceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'category_id' => $this->faker->numberBetween(1, Category::count()),
+            'user_id' => $this->faker->numberBetween(1, User::count()),
+            'latitude' => $this->faker->randomFloat(6, -15.85, -15.69),
+            'longitude' => $this->faker->randomFloat(6, -47.78, -47.95),
+            'address' => $this->faker->address,
+            'website' => $this->faker->url,
+            'phone' => $this->faker->phoneNumber,
+            'cover' => 'https://dummyimage.com/380x200/333/fff',
+            'approved' => $this->faker->boolean(50)
+        ];
+    }
+}
