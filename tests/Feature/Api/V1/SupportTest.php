00dcfa54a06a26f452cf0153d44a5af4ac61eeda
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-08-22 07:02:49 -0300

    test: SupportTest - store

--- tests/Feature/Api/V1/SupportTest.php
+++ tests/Feature/Api/V1/SupportTest.php
@@ -6,6 +6,7 @@ use App\Http\Resources\V1\SupportCollection;
 use App\Http\Resources\V1\SupportResource;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class SupportTest extends TestCase
@@ -59,6 +60,37 @@ class SupportTest extends TestCase
                 'userId',
                 'message',
                 'createdAt'
+            ])->json();
+    }
+
+    public function test_user_cannot_show_support()
+    {
+        $this->authUser();
+
+        $support = $this->createSupport();
+
+        $this->withExceptionHandling();
+
+        //assert that it throws AuthorizationException
+        $this->getJson(route('supports.show', $support->id))
+            ->assertStatus(401)
+            ->assertJsonStructure([
+                'message'
+            ]);
+    }
+
+    public function test_store_support()
+    {
+        $this->authAdmin();
+
+        $this->postJson(route('supports.store'), [
+            'userId' => Auth::user()->id,
+            'message' => 'test'
+        ])->assertOk()
+            ->assertJsonStructure([
+                'id',
+                'user_id',
+                'message'
             ])->json();
     }
 }
