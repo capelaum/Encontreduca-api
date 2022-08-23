9eeb09e6c295985cfe1499e3a404b2e0fca2b128
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-08-23 18:48:51 -0300

    remove unecessary code

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -28,15 +28,4 @@ class Review extends Model
     {
         return $this->belongsTo(Resource::class);
     }
-
-    /**
-     * Prepare a date for array / JSON serialization.
-     *
-     * @param DateTimeInterface $date
-     * @return string
-     */
-    protected function serializeDate(DateTimeInterface $date)
-    {
-        return $date->format('d/m/Y');
-    }
 }
